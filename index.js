2dcdbd38c5e418b9d4542973749a2a2ce636db6e
Felipe Tunes <felipetunes@gmail.com>
2020-08-17 13:15:42 -0300

    log

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ exports["cloud-functions-test"] = (req, res) => {
 
    fs.readFile('jslt', function(err, template) {
       let message = template.toString();
+      console.log(message);
       res.status(200).send(message);
    });
 
