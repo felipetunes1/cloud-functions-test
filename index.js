6c701efe7bb4e0cb0b878aab4511db12a8c497e8
Felipe Tunes <felipetunes@gmail.com>
2020-08-17 13:55:25 -0300

    Parse Json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let fs = require('fs');
 exports["cloud-functions-test"] = (req, res) => {
 
    fs.readFile('jslt', function(err, template) {
-      let message = jslt.transform(req.body, { $fetch: JSON.parse(template.toString()) });
+      let message = JSON.parse(jslt.transform(req.body, { $fetch: JSON.parse(template.toString()) }));
       console.log(message);
       res.status(200).json(message);
    });
