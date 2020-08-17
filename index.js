cb63fe9b2a5a40ab8223da84ca9a06372f19b5a7
Felipe Tunes <felipetunes@gmail.com>
2020-08-17 15:11:24 -0300

    escape

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ let fs = require('fs');
 exports["cloud-functions-test"] = (req, res) => {
    console.log(req.body.request);
    console.log(req.body.template);
+   let template = unescape(req.body.template)
 
-   let message = JSON.parse(jslt.transform(req.body.request, { $fetch: JSON.parse(req.body.template) }));
+   let message = JSON.parse(jslt.transform(req.body.request, { $fetch: JSON.parse(template) }));
    console.log(message);
    res.status(200).json(message);
 
