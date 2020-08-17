e7f7782b78ddd0400503b93a4d2e59ca74ba8ea2
Felipe Tunes <felipetunes@gmail.com>
2020-08-17 14:54:33 -0300

    Input Template

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ let fs = require('fs');
 
 exports["cloud-functions-test"] = (req, res) => {
 
-   fs.readFile('jslt', function(err, template) {
-      let message = JSON.parse(jslt.transform(req.body, { $fetch: JSON.parse(template.toString()) }));
-      console.log(message);
-      res.status(200).json(message);
-   });
+   let message = JSON.parse(jslt.transform(req.body.request, { $fetch: JSON.parse(req.body.template) }));
+   console.log(message);
+   res.status(200).json(message);
 
 }
