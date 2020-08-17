e6cb420b5b65353b13114d5557a55aeb20da80c4
Felipe Tunes <felipetunes@gmail.com>
2020-08-17 15:24:49 -0300

    retornar codigo

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ var jslt = require("jslt");
 let fs = require('fs');
 
 exports["cloud-functions-test"] = (req, res) => {
-   console.log(req.body.request);
-   console.log(req.body.template);
-   let template = unescape(req.body.template)
-   console.log(template);
 
-   let message = JSON.parse(jslt.transform(req.body.request, { $fetch: JSON.parse(template) }));
-   console.log(message);
-   res.status(200).json(message);
+   fs.readFile('jslt', function(err, template) {
+      let message = jslt.transform(req.body, { $fetch: JSON.parse(template.toString()) });
+      console.log(JSON.parse(message));
+      res.status(200).json(JSON.parse(message));
+   });
 
 }
