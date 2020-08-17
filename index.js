804e22419acadb604411bfd795db0705ec4c569b
Felipe Tunes <felipetunes@gmail.com>
2020-08-17 13:07:04 -0300

    file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let fs = require('fs');
 
 exports["cloud-functions-test"] = (req, res) => {
 
-   fs.readFile('demofile1.html', function(err, template) {
+   fs.readFile('jslt', function(err, template) {
       let message = template;
       res.status(200).send(message);
    });
