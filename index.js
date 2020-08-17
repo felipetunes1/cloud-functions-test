1eef66246f30fbc0da36d2d34c80ac89faef97d1
Felipe Tunes <felipetunes@gmail.com>
2020-08-17 13:35:36 -0300

    Mudar JSLT

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ let fs = require('fs');
 exports["cloud-functions-test"] = (req, res) => {
 
    fs.readFile('jslt', function(err, template) {
-      let message = jslt.transform(req.body, { $fetch: template.toString() });
-      console.log(message);
-      res.status(200).json(message);
+      let message = jslt.transform(req.body, { $fetch: JSON.parse(template.toString()) });
+      console.log(JSON.parse(message));
+      res.status(200).json(JSON.parse(message));
    });
 
 }
